26efee8355c2c9307a759f5588a09130c6aab1ff
paulobusato <paulo.busato2@gmail.com>
2019-10-13 18:08:40 +0000

    modificado cabecalho e rodape para serem reutilizados

---
+++ visao/compartilhado/cabecalho/cabecalho.php
@@ -0,0 +1,9 @@
+<header class="cabecalho-principal">
+    <nav class="cabecalho-principal__navegacao">
+        <h1>
+            <a href="/index.html" class="cabecalho-principal__navegacao__titulo">eComida</a>
+        </h1>
+        <a href="compartilhado/login.html" class="btn-entrar cabecalho-principal__navegacao__entrar">Entrar</a>
+        <a href="compartilhado/login.html" class="icon-entrar"><i class="material-icons">account_circle</i></a>
+    </nav>
+</header>
