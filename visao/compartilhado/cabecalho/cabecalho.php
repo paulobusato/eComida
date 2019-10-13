8caaa47135ee82cbe4491b03302bb9e3be4f05ea
paulobusato <paulo.busato2@gmail.com>
2019-10-13 19:12:09 +0000

    corrigido links cabecalho

--- visao/compartilhado/cabecalho/cabecalho.php
+++ visao/compartilhado/cabecalho/cabecalho.php
@@ -3,7 +3,7 @@
         <h1>
             <a href="/index.html" class="cabecalho-principal__navegacao__titulo">eComida</a>
         </h1>
-        <a href="compartilhado/login.html" class="btn-entrar cabecalho-principal__navegacao__entrar">Entrar</a>
-        <a href="compartilhado/login.html" class="icon-entrar"><i class="material-icons">account_circle</i></a>
+        <a href="/visao/usuarios/login/login.php" class="btn-entrar cabecalho-principal__navegacao__entrar">Entrar</a>
+        <a href="/visao/usuarios/login/login.php" class="icon-entrar"><i class="material-icons">account_circle</i></a>
     </nav>
 </header>
