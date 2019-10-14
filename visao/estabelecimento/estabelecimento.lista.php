27f61a00bdf816a89ad8994838ce8bf004bcb39d
paulobusato <paulo.busato2@gmail.com>
2019-10-14 22:32:59 +0000

    corrigido modo deskstop

--- visao/estabelecimento/estabelecimento.lista.php
+++ visao/estabelecimento/estabelecimento.lista.php
@@ -21,43 +21,43 @@
         <h1 class="cabecalho__titulo">Salada</h1>
         <hr class="cabecalho__divider">
     </header>
-    <!-- <section class="sacola-container display--none">
+    <section class="sacola-container display--none">
         <p class="sacola__texto">Seu pedido em</p>
         <h3 class="sacola__estabelecimento">Number One Chicken</h3>
-                <hr class="sacola__divider">
-                <section class="sacola-pedidos">
-                    <article class="sacola-pedido">
-                        <p class="sacola-pedido__descricao"><span>1</span>x Combo p - <span>indicamos para 1 a 2 pessoas</span></p>
-                        <p class="sacola-pedido__ingredientes">
-                        <span>1x Tiras de filé de coxa e sobrecoxa</span>,
-                        <span>1x Arroz branco g</span>,
-                        <span>1x Maionese verde</span>
-                        </p>
-                        <p class="sacola-pedido__preco">R$ 32,99</p>
-                        <nav class="sacola-pedido__actions">
-                        <button class="sacola-pedido__editar"><i class="material-icons">edit</i></button>
-                        <button class="sacola-pedido__remover"><i class="material-icons">remove_circle_outline</i></button>
-                        </nav>
-                    </article>
-                    <hr class="sacola__divider">
-                </section>
-                <section class="sacola-pedidos">
-                    <article class="sacola-pedido">
-                        <p class="sacola-pedido__descricao"><span>1</span>x Combo p - <span>indicamos para 1 a 2 pessoas</span></p>
-                        <p class="sacola-pedido__ingredientes">
-                        <span>1x Tiras de filé de coxa e sobrecoxa</span>,
-                        <span>1x Arroz branco g</span>,
-                        <span>1x Maionese verde</span>
-                        </p>
-                        <p class="sacola-pedido__preco">R$ 32,99</p>
-                        <nav class="sacola-pedido__actions">
-                        <button class="sacola-pedido__editar"><i class="material-icons">edit</i></button>
-                        <button class="sacola-pedido__remover"><i class="material-icons">remove_circle_outline</i></button>
-                        </nav>
-                    </article>
-                    <hr class="sacola__divider">
-                </section>
-            </section> -->
+        <hr class="sacola__divider">
+        <section class="sacola-pedidos">
+            <article class="sacola-pedido">
+                <p class="sacola-pedido__descricao"><span>1</span>x Combo p - <span>indicamos para 1 a 2 pessoas</span></p>
+                <p class="sacola-pedido__ingredientes">
+                <span>1x Tiras de filé de coxa e sobrecoxa</span>,
+                <span>1x Arroz branco g</span>,
+                <span>1x Maionese verde</span>
+                </p>
+                <p class="sacola-pedido__preco">R$ 32,99</p>
+                <nav class="sacola-pedido__actions">
+                <button class="sacola-pedido__editar"><i class="material-icons">edit</i></button>
+                <button class="sacola-pedido__remover"><i class="material-icons">remove_circle_outline</i></button>
+                </nav>
+            </article>
+            <hr class="sacola__divider">
+        </section>
+        <section class="sacola-pedidos">
+            <article class="sacola-pedido">
+                <p class="sacola-pedido__descricao"><span>1</span>x Combo p - <span>indicamos para 1 a 2 pessoas</span></p>
+                <p class="sacola-pedido__ingredientes">
+                <span>1x Tiras de filé de coxa e sobrecoxa</span>,
+                <span>1x Arroz branco g</span>,
+                <span>1x Maionese verde</span>
+                </p>
+                <p class="sacola-pedido__preco">R$ 32,99</p>
+                <nav class="sacola-pedido__actions">
+                <button class="sacola-pedido__editar"><i class="material-icons">edit</i></button>
+                <button class="sacola-pedido__remover"><i class="material-icons">remove_circle_outline</i></button>
+                </nav>
+            </article>
+            <hr class="sacola__divider">
+        </section>
+    </section>
     <section class="estabelecimento-list">
         <article class="estabelecimento-list__item">
             <a class="estabelecimento__link" href="/visao/estabelecimento/estabelecimento.produtos.php">
