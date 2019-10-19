58ee40997d05ecd8218b3fd979ec42a5c1e24910
paulobusato <paulo.busato2@gmail.com>
2019-10-19 17:42:09 +0000

    alterado estrutura codigo menu

--- visao/compartilhado/menu/menu.php
+++ visao/compartilhado/menu/menu.php
@@ -1,5 +1,11 @@
 <nav class="menu">
-    <button type="button" class="menu__link" id="btnIniciar"><i class="material-icons menu__icon">home</i></button>
-    <button type="button" class="menu__link" id="btnBuscar"><i class="material-icons menu__icon">search</i></button>
-    <button type="button" class="menu__link" id="btnSacola"><i class="material-icons menu__icon">shopping_basket</i></button>
+    <button type="button" class="menu__link" id="btnIniciar">
+        <i class="material-icons menu__icon">home</i>
+    </button>
+    <button type="button" class="menu__link" id="btnBuscar">
+        <i class="material-icons menu__icon">search</i>
+    </button>
+    <button type="button" class="menu__link" id="btnSacola">
+        <i class="material-icons menu__icon">shopping_basket</i>
+    </button>
 </nav>
