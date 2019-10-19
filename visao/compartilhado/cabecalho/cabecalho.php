c665bf54eacd1500573c8aabf6dd7a5fa0a16f0d
paulobusato <paulo.busato2@gmail.com>
2019-10-19 16:25:06 +0000

    separacao entre cabecalho e navegacao

--- visao/compartilhado/cabecalho/cabecalho.php
+++ visao/compartilhado/cabecalho/cabecalho.php
@@ -1,9 +1,4 @@
 <header class="cabecalho">
-    <nav class="cabecalho__navegacao">
-        <i class="material-icons voltar__icon">keyboard_arrow_left</i>
-        <a class="cabecalho__logo" href="/index.html">eComida</a>
-        <button class="sacola-button"><i class="material-icons sacola-button__icon">shopping_cart</i></button>
-    </nav>
     <h1 class="cabecalho__titulo">Salada</h1>
     <hr class="cabecalho__divider">
 </header>
---
+++ visao/compartilhado/navegacao/navegacao.php
@@ -0,0 +1,5 @@
+<nav class="cabecalho__navegacao">
+    <i class="material-icons voltar__icon">keyboard_arrow_left</i>
+    <a class="cabecalho__logo" href="/index.html">eComida</a>
+    <button class="sacola-button"><i class="material-icons sacola-button__icon">shopping_cart</i></button>
+</nav>
