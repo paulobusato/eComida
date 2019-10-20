ac122600025c03d1e6242ef6de150e56bf600bc7
paulobusato <paulo.busato2@gmail.com>
2019-10-20 21:35:15 +0000

    corrigido layout login mobile desktop

--- visao/usuarios/login/login.php
+++ visao/usuarios/login/login.php
@@ -6,14 +6,22 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-  <link rel="stylesheet" href="/visao/compartilhado/cabecalho/cabecalho.css">
   <link rel="stylesheet" href="/visao/compartilhado/rodape/rodape.css">
   <link rel="stylesheet" href="login.css">
   <title>Login - eComida</title>
 </head>
 <body>
   <section class="imagem-fundo"></section>
-  <?php require_once('/workspaces/eComida/visao/compartilhado/cabecalho/cabecalho.php'); ?>
+  <header class="cabecalho-principal">
+      <nav class="cabecalho-principal__navegacao">
+          <h1>
+              <a href="/index.html" class="cabecalho-principal__navegacao__titulo">eComida</a>
+          </h1>
+          <a href="/visao/usuarios/login/login.php" class="btn-entrar cabecalho-principal__navegacao__entrar">Entrar</a>
+          <a href="/visao/usuarios/login/login.php" class="icon-entrar"><i class="material-icons">account_circle</i></a>
+      </nav>
+      <hr class="cabecalho-principal__divisor">
+  </header>
   <main class="container animated bounceInLeft">
     <section class="cadastro">
       <h2 class="cadastro-titulo">Bem-vindo novamente</h2>
